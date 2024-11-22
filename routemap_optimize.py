65ccdf13a3f9971e916ce471b27e064f0ae98502
rejipmathew <Rejipmathew@users.noreply.github.com>
2024-11-22 10:49:07 -0500

    Update routemap_optimize.py

--- routemap_optimize.py
+++ routemap_optimize.py
@@ -112,7 +112,7 @@ def main():
     with tab1:
         st.header("Welcome to the Route Optimization App")
         st.write(""" - Home: Introduction and navigation instructions. 
-        - Addresses: Enter the addresses you want to optimize the route for.
+        - Addresses: Enter the addresses you want to optimize the route. To upload csv file add a header with addresss and enter address in each row.
         - Map: View the optimized route on the map. 
         - Route Table: See the detailed route and distances between stops. """)
         if st.button("Go to Addresses Tab"):
