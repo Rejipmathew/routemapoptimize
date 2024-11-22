2a4ce889744e469e2fea5516e0105d571655c103
rejipmathew <Rejipmathew@users.noreply.github.com>
2024-11-22 12:50:30 -0500

    Update routemap_optimize.py

--- routemap_optimize.py
+++ routemap_optimize.py
@@ -122,7 +122,7 @@ def main():
     with tab1:
         st.header("Welcome to the Route Map Optimization App:sunglasses:")
         st.write(""" - # Home: Introduction and navigation instructions.
-        - ## Addresses: Enter the addresses you want to optimize the route with first address as starting and last address as ending point.Clear button clear default address in the search box.
+        - ##  Addresses: Enter the addresses you want to optimize the route with first address as starting and last address as ending point.Clear button clear default address in the search box.
         - ## Map: View the optimized route on the map.
         - ## Route Table: See the detailed route and distances between stops.
         -### Preview driving direction takes to Google Maps with input address.""")
